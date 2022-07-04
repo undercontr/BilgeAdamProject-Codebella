39bd7619f9f2085867f2950909f45e394145e2f5
Önder Alkan <onder.alkan15@gmail.com>
2022-07-04 23:11:50 +0300

    Some of the entities are modified and new logics added like Like.

--- Entity/Author.cs
+++ Entity/Author.cs
@@ -4,8 +4,8 @@ namespace Entity;
 
 public class Author: BaseEntity<User>
 {
-    public ICollection<Article> Articles { get; set; }
     public string Nickname { get; set; }
     public string FullName { get; set; }
     public User UserInfo { get; set; }
+    public ICollection<Article> Articles { get; set; }
 }
